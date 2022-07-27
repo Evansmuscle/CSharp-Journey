122c98974242b2313ff900f3fa6e26b3dbc6f0bd
Evansmuscle <yigit.korkmaz@cultofcoders.com>
2022-07-27 16:14:55 +0300

    Learn how Threads work

---
+++ threads/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using System;
+using System.Threading;
+
+namespace threads
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread mainThread = Thread.CurrentThread;
+
+            Thread threadOne = new Thread(() => CountUp(0, 10));
+            Thread threadTwo = new Thread(() => CountDown(10, 0));
+
+            threadOne.Start();
+            threadTwo.Start();
+        }
+
+        public static void CountUp(int countFrom, int countTo)
+        {
+            for (int i = countFrom; i < (countTo + 1); i++)
+            {
+                Console.WriteLine($"Current number: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void CountDown(int countFrom, int countTo)
+        {
+            for (int i = countFrom; i > (countTo - 1); i--)
+            {
+                Console.WriteLine($"Current number: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
